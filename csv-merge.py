601141152cf8f305ed5669a4290e796b36ae9333
Clayton Liddell <clayton.liddell@civicactions.com>
2021-10-29 16:27:02 -0500

    Import CSV values as string to preserve formatting

--- csv-merge.py
+++ csv-merge.py
@@ -10,7 +10,7 @@ def main(args: argparse.Namespace) -> None:
 
 # Combine the supplied CSV files and write the output to the given output file.
 def combine_csvs(files: [str], output) -> None:
-    dfs = (pd.read_csv(f, sep=',') for f in files)
+    dfs = (pd.read_csv(f, sep=',', dtype=str) for f in files)
     df_merged = pd.concat(dfs, ignore_index=True)
     df_merged.to_csv(output, index=False)
 
