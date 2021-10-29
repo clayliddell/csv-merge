88f3bee504a6c7bade412209fca33b6c706bedb3
Clayton Liddell <clayton.liddell@civicactions.com>
2021-10-29 16:19:48 -0500

    Fix bug; allow omitance of output option.

--- csv-merge.py
+++ csv-merge.py
@@ -2,20 +2,22 @@
 
 import argparse
 import pandas as pd
+import sys
 
 def main(args: argparse.Namespace) -> None:
-   combine_csvs(files, output_file) 
+    output = args.output_file if args.output_file is not None else sys.stdout
+    combine_csvs(args.files, output) 
 
 # Combine the supplied CSV files and write the output to the given output file.
-def combine_csvs(files: [str], output_file) -> None:
+def combine_csvs(files: [str], output) -> None:
     dfs = (pd.read_csv(f, sep=',') for f in files)
     df_merged = pd.concat(dfs, ignore_index=True)
-    df_merged.to_csv(output_file)
+    df_merged.to_csv(output, index=False)
 
 # Construct argument parser.
 parser = argparse.ArgumentParser(description='This script merges CSV files with shared columns.')
-parser.add_argument('files', type=str, nargs='+', help='paths to CSV files to be merged.')
-parser.add_argument('-o', '--output_file', type=str, help='output file')
+parser.add_argument('files', metavar='FILE', nargs='+', type=str, help='path to CSV file being merged')
+parser.add_argument('-o', '--output_file', nargs='?', type=str, help='output file')
 
 # If this script is being called directly, parse the supplied arguments and run
 # the main function.
