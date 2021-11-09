ac86ff2c14c8e40509834b730236145baf4354a1
Clayton Liddell <clayton.liddell@civicactions.com>
2021-11-09 16:42:27 -0600

    Remove unnecessary import.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
     name='csv-merge',
-    version='0.1.0',
+    version='0.1.1',
     description='A tool for combining CSV files.',
     author='Clayton Liddell',
     author_email='account+github@clayliddell.com',
