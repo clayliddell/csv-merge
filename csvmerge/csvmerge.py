ac9d2ccd3b5979c8f5e3f118b3fe98c80a65b2dd
Clayton Liddell <clayton.liddell@civicactions.com>
2021-12-22 11:24:22 -0600

    Fix 'na', 'nan', and 'null' being read as NaN

--- csvmerge/csvmerge.py
+++ csvmerge/csvmerge.py
@@ -2,6 +2,6 @@ import pandas as pd
 
 # Combine the supplied CSV files and write the output to the given output file.
 def merge(files: [str], output) -> None:
-    dfs = (pd.read_csv(f, sep=',', dtype=str) for f in files)
+    dfs = (pd.read_csv(f, sep=',', dtype=str, keep_default_na=False) for f in files)
     df_merged = pd.concat(dfs, ignore_index=True)
     df_merged.to_csv(output, index=False)
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='csv-merge',
-    version='0.1.1',
+    version='0.1.2',
     description='A tool for combining CSV files.',
     author='Clayton Liddell',
     author_email='account+github@clayliddell.com',
